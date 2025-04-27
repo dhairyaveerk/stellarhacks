5c87acf8deffb0a8eef2b82cacebd8a867f298db
dhairyaveerk <dhairyakochar14@gmail.com>
2025-04-26 18:54:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ function generateAIComment(entry) {
     
     // Check for stress-related words
     else if (stressWords.some(word => entry.toLowerCase().includes(word))) {
-        aiComment = 'It seems like you’re feeling a bit stressed. Try taking deep breaths and doing something relaxing!';
+        aiComment = 'It seems like you’re feeling a bit stressed. Try taking deep breaths and playing our soothing games!';
     }
 
     return aiComment;
